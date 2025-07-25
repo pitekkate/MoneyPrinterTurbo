5aacf0accf9c2bd4bd063f0ce0b9067985ab314b
pitekkate <kangristiktok@gmail.com>
2025-07-25 14:47:31 +0700

    Update llm.py

--- app/services/llm.py
+++ app/services/llm.py
@@ -1,14 +1,19 @@
-import json
+            import json
 import logging
 import re
 import requests
 from typing import List
+
 import g4f
 from loguru import logger
 from openai import AzureOpenAI, OpenAI
 from openai.types.chat import ChatCompletion
+
 from app.config import config
+
 _max_retries = 5
+
+
 def _generate_response(prompt: str) -> str:
     try:
         content = ""
@@ -22,8 +27,10 @@ def _generate_response(prompt: str) -> str:
                 model=model_name,
                 messages=[{"role": "user", "content": prompt}],
             )
-        
-        elif llm_provider == "openrouter":
+            elif llm_provider == "openrouter":
+            import os
+            import requests
+
             api_key = config.app.get("openrouter_api_key")
             model_name = config.app.get("openrouter_model_name")
             base_url = config.app.get("openrouter_base_url", "")
@@ -31,11 +38,11 @@ def _generate_response(prompt: str) -> str:
             # Validasi konfigurasi
             if not api_key:
                 raise ValueError(
-                    f"{llm_provider}: api_key is not set, please set it in the config.toml file."
+                    f"{llm_provider}: api_key tidak diatur, silakan atur di file config.toml."
                 )
             if not model_name:
                 raise ValueError(
-                    f"{llm_provider}: model_name is not set, please set it in the config.toml file."
+                    f"{llm_provider}: model_name tidak diatur, silakan atur di file config.toml."
                 )
             
             # Gunakan base_url default jika tidak diset
@@ -46,8 +53,8 @@ def _generate_response(prompt: str) -> str:
             headers = {
                 "Authorization": f"Bearer {api_key}",
                 "Content-Type": "application/json",
-                "HTTP-Referer": "https://yourdomain.com",  # Opsional, ganti dengan domain Anda
-                "X-Title": "MoneyPrinterTurbo",  # Opsional
+                "HTTP-Referer": os.environ.get("YOUR_SITE_URL", "https://yourdomain.com"), # Opsional
+                "X-Title": os.environ.get("YOUR_APP_NAME", "MoneyPrinterTurbo"), # Opsional
             }
             
             # Data payload untuk permintaan
@@ -71,10 +78,9 @@ def _generate_response(prompt: str) -> str:
                 result = response.json()
                 content = result['choices'][0]['message']['content'].strip()
             except requests.exceptions.RequestException as e:
-                raise Exception(f"[{llm_provider}] request failed: {str(e)}")
+                raise Exception(f"[{llm_provider}] permintaan gagal: {str(e)}")
             except KeyError as e:
-                raise Exception(f"[{llm_provider}] returned an invalid response format: {str(e)}")
-        
+                raise Exception(f"[{llm_provider}] mengembalikan format respons yang tidak valid: {str(e)}")
         else:
             api_version = ""  # for azure
             if llm_provider == "moonshot":
@@ -137,6 +143,7 @@ def _generate_response(prompt: str) -> str:
                     if not base_url:
                         base_url = "https://text.pollinations.ai/openai"
                     model_name = config.app.get("pollinations_model_name", "openai-fast")
+                   
                     # Prepare the payload
                     payload = {
                         "model": model_name,
@@ -145,27 +152,33 @@ def _generate_response(prompt: str) -> str:
                         ],
                         "seed": 101  # Optional but helps with reproducibility
                     }
+                    
                     # Optional parameters if configured
                     if config.app.get("pollinations_private"):
                         payload["private"] = True
                     if config.app.get("pollinations_referrer"):
                         payload["referrer"] = config.app.get("pollinations_referrer")
+                    
                     headers = {
                         "Content-Type": "application/json"
                     }
+                    
                     # Make the API request
                     response = requests.post(base_url, headers=headers, json=payload)
                     response.raise_for_status()
                     result = response.json()
+                    
                     if result and "choices" in result and len(result["choices"]) > 0:
                         content = result["choices"][0]["message"]["content"]
                         return content.replace("\n", "")
                     else:
                         raise Exception(f"[{llm_provider}] returned an invalid response format")
+                        
                 except requests.exceptions.RequestException as e:
                     raise Exception(f"[{llm_provider}] request failed: {str(e)}")
                 except Exception as e:
                     raise Exception(f"[{llm_provider}] error: {str(e)}")
+
             if llm_provider not in ["pollinations", "ollama"]:  # Skip validation for providers that don't require API key
                 if not api_key:
                     raise ValueError(
@@ -179,9 +192,11 @@ def _generate_response(prompt: str) -> str:
                     raise ValueError(
                         f"{llm_provider}: base_url is not set, please set it in the config.toml file."
                     )
+
             if llm_provider == "qwen":
                 import dashscope
                 from dashscope.api_entities.dashscope_response import GenerationResponse
+
                 dashscope.api_key = api_key
                 response = dashscope.Generation.call(
                     model=model_name, messages=[{"role": "user", "content": prompt}]
@@ -193,6 +208,7 @@ def _generate_response(prompt: str) -> str:
                             raise Exception(
                                 f'[{llm_provider}] returned an error response: "{response}"'
                             )
+
                         content = response["output"]["text"]
                         return content.replace("\n", "")
                     else:
@@ -201,15 +217,19 @@ def _generate_response(prompt: str) -> str:
                         )
                 else:
                     raise Exception(f"[{llm_provider}] returned an empty response")
+
             if llm_provider == "gemini":
                 import google.generativeai as genai
+
                 genai.configure(api_key=api_key, transport="rest")
+
                 generation_config = {
                     "temperature": 0.5,
                     "top_p": 1,
                     "top_k": 1,
                     "max_output_tokens": 2048,
                 }
+
                 safety_settings = [
                     {
                         "category": "HARM_CATEGORY_HARASSMENT",
@@ -228,18 +248,22 @@ def _generate_response(prompt: str) -> str:
                         "threshold": "BLOCK_ONLY_HIGH",
                     },
                 ]
+
                 model = genai.GenerativeModel(
                     model_name=model_name,
                     generation_config=generation_config,
                     safety_settings=safety_settings,
                 )
+
                 try:
                     response = model.generate_content(prompt)
                     candidates = response.candidates
                     generated_text = candidates[0].content.parts[0].text
                 except (AttributeError, IndexError) as e:
                     print("Gemini Error:", e)
+
                 return generated_text
+
             if llm_provider == "cloudflare":
                 response = requests.post(
                     f"https://api.cloudflare.com/client/v4/accounts/{account_id}/ai/run/{model_name}",
@@ -257,6 +281,7 @@ def _generate_response(prompt: str) -> str:
                 result = response.json()
                 logger.info(result)
                 return result["result"]["response"]
+
             if llm_provider == "ernie":
                 response = requests.post(
                     "https://aip.baidubce.com/oauth/2.0/token", 
@@ -268,6 +293,7 @@ def _generate_response(prompt: str) -> str:
                 )
                 access_token = response.json().get("access_token")
                 url = f"{base_url}?access_token={access_token}"
+
                 payload = json.dumps(
                     {
                         "messages": [{"role": "user", "content": prompt}],
@@ -280,10 +306,12 @@ def _generate_response(prompt: str) -> str:
                     }
                 )
                 headers = {"Content-Type": "application/json"}
+
                 response = requests.request(
                     "POST", url, headers=headers, data=payload
                 ).json()
                 return response.get("result")
+
             if llm_provider == "azure":
                 client = AzureOpenAI(
                     api_key=api_key,
@@ -295,6 +323,7 @@ def _generate_response(prompt: str) -> str:
                     api_key=api_key,
                     base_url=base_url,
                 )
+
             response = client.chat.completions.create(
                 model=model_name, messages=[{"role": "user", "content": prompt}]
             )
@@ -310,16 +339,21 @@ def _generate_response(prompt: str) -> str:
                 raise Exception(
                     f"[{llm_provider}] returned an empty response, please check your network connection and try again."
                 )
+
         return content.replace("\n", "")
     except Exception as e:
         return f"Error: {str(e)}"
+
+
 def generate_script(
     video_subject: str, language: str = "", paragraph_number: int = 1
 ) -> str:
     prompt = f"""
 # Role: Video Script Generator
+
 ## Goals:
 Generate a script for a video, depending on the subject of the video.
+
 ## Constrains:
 1. the script is to be returned as a string with the specified number of paragraphs.
 2. do not under any circumstance reference this prompt in your response.
@@ -329,28 +363,36 @@ Generate a script for a video, depending on the subject of the video.
 6. do not include "voiceover", "narrator" or similar indicators of what should be spoken at the beginning of each paragraph or line.
 7. you must not mention the prompt, or anything about the script itself. also, never talk about the amount of paragraphs or lines. just write the script.
 8. respond in the same language as the video subject.
+
 # Initialization:
 - video subject: {video_subject}
 - number of paragraphs: {paragraph_number}
 """.strip()
     if language:
         prompt += f"\n- language: {language}"
+
     final_script = ""
     logger.info(f"subject: {video_subject}")
+
     def format_response(response):
         # Clean the script
         # Remove asterisks, hashes
         response = response.replace("*", "")
         response = response.replace("#", "")
+
         # Remove markdown syntax
         response = re.sub(r"\[.*\]", "", response)
         response = re.sub(r"\(.*\)", "", response)
+
         # Split the script into paragraphs
-        paragraphs = response.split("\n")
+        paragraphs = response.split("\n\n")
+
         # Select the specified number of paragraphs
         # selected_paragraphs = paragraphs[:paragraph_number]
+
         # Join the selected paragraphs into a single string
-        return "\n".join(paragraphs)
+        return "\n\n".join(paragraphs)
+
     for i in range(_max_retries):
         try:
             response = _generate_response(prompt=prompt)
@@ -358,13 +400,16 @@ Generate a script for a video, depending on the subject of the video.
                 final_script = format_response(response)
             else:
                 logging.error("gpt returned an empty response")
+
             # g4f may return an error message
             if final_script and "当日额度已消耗完" in final_script:
                 raise ValueError(final_script)
+
             if final_script:
                 break
         except Exception as e:
             logger.error(f"failed to generate script: {e}")
+
         if i < _max_retries:
             logger.warning(f"failed to generate video script, trying again... {i + 1}")
     if "Error: " in final_script:
@@ -372,27 +417,37 @@ Generate a script for a video, depending on the subject of the video.
     else:
         logger.success(f"completed: \n{final_script}")
     return final_script.strip()
+
+
 def generate_terms(video_subject: str, video_script: str, amount: int = 5) -> List[str]:
     prompt = f"""
 # Role: Video Search Terms Generator
+
 ## Goals:
 Generate {amount} search terms for stock videos, depending on the subject of a video.
+
 ## Constrains:
 1. the search terms are to be returned as a json-array of strings.
 2. each search term should consist of 1-3 words, always add the main subject of the video.
 3. you must only return the json-array of strings. you must not return anything else. you must not return the script.
 4. the search terms must be related to the subject of the video.
 5. reply with english search terms only.
+
 ## Output Example:
 ["search term 1", "search term 2", "search term 3","search term 4","search term 5"]
+
 ## Context:
 ### Video Subject
 {video_subject}
+
 ### Video Script
 {video_script}
+
 Please note that you must use English for generating video search terms; Chinese is not accepted.
 """.strip()
+
     logger.info(f"subject: {video_subject}")
+
     search_terms = []
     response = ""
     for i in range(_max_retries):
@@ -407,6 +462,7 @@ Please note that you must use English for generating video search terms; Chinese
             ):
                 logger.error("response is not a list of strings.")
                 continue
+
         except Exception as e:
             logger.warning(f"failed to generate video terms: {str(e)}")
             if response:
@@ -417,12 +473,16 @@ Please note that you must use English for generating video search terms; Chinese
                     except Exception as e:
                         logger.warning(f"failed to generate video terms: {str(e)}")
                         pass
+
         if search_terms and len(search_terms) > 0:
             break
         if i < _max_retries:
             logger.warning(f"failed to generate video terms, trying again... {i + 1}")
+
     logger.success(f"completed: \n{search_terms}")
     return search_terms
+
+
 if __name__ == "__main__":
     video_subject = "生命的意义是什么"
     script = generate_script(
@@ -431,4 +491,8 @@ if __name__ == "__main__":
     print("######################")
     print(script)
     search_terms = generate_terms(
-                
+        video_subject=video_subject, video_script=script, amount=5
+    )
+    print("######################")
+    print(search_terms)
+    
